499c3ee053d87f9fa36ec144c6f60bf299e8633f
moonheuiwon <wonheuimoon@gmail.com>
2020-05-27 10:51:40 +0900

    피자 가게 뿌려줄 어댑터 뼈대작업

---
+++ app/src/main/java/com/example/pizzaapplication/adapters/PizzaStoreAdapter.java
@@ -0,0 +1,42 @@
+package com.example.pizzaapplication.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.pizzaapplication.R;
+import com.example.pizzaapplication.datas.PizzaStore;
+
+import java.util.List;
+
+public class PizzaStoreAdapter extends ArrayAdapter<PizzaStore> {
+
+    Context mContext;
+    List<PizzaStore> mList;
+    LayoutInflater inf;
+
+    public PizzaStoreAdapter(@NonNull Context context, int resource, @NonNull List<PizzaStore> objects) {
+        super(context, resource, objects);
+
+        mContext = context;
+        mList = objects;
+        inf = LayoutInflater.from(mContext);
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View row = convertView;
+
+        if (row == null) {
+            row = inf.inflate(R.layout.pizza_store_item, null);
+        }
+        return row;
+    }
+}
