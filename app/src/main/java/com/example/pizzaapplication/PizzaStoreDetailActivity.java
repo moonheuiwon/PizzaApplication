3e5cac133aabf293006189d084285fac9035dae9
moonheuiwon <wonheuimoon@gmail.com>
2020-05-27 13:15:37 +0900

    피자가게 전호 걸기 기능을 추가했다

--- app/src/main/java/com/example/pizzaapplication/PizzaStoreDetailActivity.java
+++ app/src/main/java/com/example/pizzaapplication/PizzaStoreDetailActivity.java
@@ -3,11 +3,20 @@ package com.example.pizzaapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import com.bumptech.glide.Glide;
 import com.example.pizzaapplication.databinding.ActivityPizzaStoreDetailBinding;
 import com.example.pizzaapplication.datas.PizzaStore;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.security.PublicKey;
+import java.util.List;
 
 public class PizzaStoreDetailActivity extends BaseActivity {
 
@@ -25,6 +34,38 @@ public class PizzaStoreDetailActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+
+        binding.callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                PermissionListener pl = new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted() {
+
+                        String phoneNum = binding.phoneNumTxt.getText().toString();
+                        Uri myUri = Uri.parse(String.format("tel:%s", phoneNum));
+                        Intent myIntent = new Intent(Intent.ACTION_CALL, myUri);
+                        startActivity(myIntent);
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(List<String> deniedPermissions) {
+
+                    }
+                };
+
+                TedPermission
+                        .with(mContext)
+                        .setPermissionListener(pl)
+                        .setDeniedMessage("거부하면 통화가 불가능함. \n 설정에서 권한을 켜주세요.")
+//                        줄바꿈은 \n 으로 가능함.
+                        .setPermissions(Manifest.permission.CALL_PHONE)
+                        .check();
+
+            }
+        });
 
     }
 
