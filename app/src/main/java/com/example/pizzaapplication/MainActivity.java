184196809d90081d01d018247d4a5c2656c3bb8d
moonheuiwon <wonheuimoon@gmail.com>
2020-05-27 10:39:18 +0900

    피자가게 데이터 클래스 추가  - 그림의 경로를 저장할 String 변수 생성

---
+++ app/src/main/java/com/example/pizzaapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.pizzaapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.example.pizzaapplication.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseActivity {
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
