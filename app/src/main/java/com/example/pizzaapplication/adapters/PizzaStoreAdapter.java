116ab52a03970d29190d3365e61cf5a4a5286915
moonheuiwon <wonheuimoon@gmail.com>
2020-05-27 11:11:00 +0900

    어댑터에서 글라이드로 이미지 출력

--- app/src/main/java/com/example/pizzaapplication/adapters/PizzaStoreAdapter.java
+++ app/src/main/java/com/example/pizzaapplication/adapters/PizzaStoreAdapter.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.bumptech.glide.Glide;
 import com.example.pizzaapplication.R;
 import com.example.pizzaapplication.datas.PizzaStore;
 
@@ -46,6 +47,8 @@ public class PizzaStoreAdapter extends ArrayAdapter<PizzaStore> {
         PizzaStore data = mList.get(position);
 
         storeNameTxt.setText(data.getName());
+
+        Glide.with(mContext).load(data.getLogoImgUrl()).into(storeLogoImg);
 
         return row;
     }
